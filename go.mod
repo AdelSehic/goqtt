f6450a13811bbeabab1b16648ebe49f5a0a1ea06
Adel Sehic <adelsehic95@gmail.com>
2024-12-23 18:19:11 +0100

    - Daemonized application

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.23.1
 require github.com/rs/zerolog v1.33.0
 
 require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/sevlyar/go-daemon v0.1.6 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"goqtt/logger"
 	"goqtt/server"
 	"goqtt/workers"
+	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
+
+	"github.com/sevlyar/go-daemon"
 )
 
 func main() {
@@ -15,6 +20,26 @@ func main() {
 	}
 	logger.Init(config.Logger)
 
+	cntxt := &daemon.Context{
+		PidFileName: "goqtt.pid",
+		PidFilePerm: 0644,
+		LogFileName: "goqtt.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"goqtt"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		logger.Console.Fatal().Err(err).Msg("Failed to daemonize")
+	}
+	if d != nil {
+		logger.Console.Info().Msg("Daemon process started successfully!")
+		return
+	}
+	defer cntxt.Release()
+
 	workers.GlobalPool = workers.NewPool(config.Pool)
 	workers.GlobalPool.StartWorkers(runtime.NumCPU())
 
@@ -22,6 +47,17 @@ func main() {
 	if srv == nil {
 		logger.HTTP.Panic().Err(err).Msg("Couldn't create a server")
 	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		logger.Console.Info().Msg("Shutting down server")
+		srv.Stop()
+		workers.GlobalPool.StopWorkers()
+		os.Exit(0)
+	}()
 
 	srv.Start()
 }
